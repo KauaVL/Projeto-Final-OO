0b37ba9901bad480a767796bab812bd263f1e181
KauaVL <kauavaleleao@gmail.com>
2025-02-11 03:56:03 -0300

    Banco de dados SQLite adicionado

--- app.py
+++ app.py
@@ -1,15 +1,59 @@
-from flask import Flask, redirect
-from controllers.auth_controller import auth_bp
+from flask import Flask, redirect, render_template, request, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = 'supersenha'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Registra o Blueprint de autenticação
-app.register_blueprint(auth_bp)
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    email = db.Column(db.String(150), unique=True, nullable=False, primary_key=True)
+    nome = db.Column(db.String(150), nullable=False)
+    cargo = db.Column(db.String(50), nullable=False)
+    senha = db.Column(db.String(150), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
-    return redirect('/login')
+    return render_template('home.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        senha = request.form['senha']
+        cargo = request.form['cargo']
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email já registrado. Tente fazer login.')
+        else:
+            new_user = User(nome=nome, email=email, senha=senha, cargo=cargo)
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('login'))
+    return render_template('signup.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        senha = request.form['senha']
+        user = User.query.filter_by(email=email).first()
+        if user and user.senha == senha:
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Credenciais inválidas. Tente novamente.')
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
